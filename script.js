0d872d76ac32bc60c9ada82b607d264362ed62e4
CauaBF1 <cauaborges@estudante.ufscar.br>
2024-05-27 22:50:45 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+    const html = document.querySelector('html')
+    html.classList.toggle('light')
+}
+
+
+
+/*
+SEM O TOGGLE
+
+function toggleMode(){
+    const html = document.documentElement
+    if(html.classList.contains('light')){
+        html.classList.remove('light')
+    }else{
+        html.classList.add('light')
+    }
+}
+*/
